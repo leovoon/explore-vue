df0beb94de69253d1e54dd1e2be1246d6130a6fa
Leo Voon <voonlihhaur@gmail.com>
2022-01-08 21:49:43 +0800

    feat: add parallax effect md

---
+++ src/components/ParallaxEffectDemo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="rounded max-w-xs ">
+    tilt:  {{ tilt }} <br>
+    roll: {{ roll }} <br>
+    source: {{ source }}
+  </div>
+
+  <div class="grid place-content-center mt-8">
+    <div ref="container">
+      <div
+        :style="cardStyle"
+        class=" bg-gradient-7 transition-all scale-100
+                duration-[0.15s] animate-ease-[cubic-bezier(.4,0,.2,1)]
+                rounded-lg w-15rem h-20rem"
+      >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { useParallax } from '@vueuse/core'
+
+const container = ref<HTMLDivElement | null>(null)
+const { tilt, roll, source } = useParallax(container)
+const cardStyle = computed(() => ({
+  border: '1px solid #cdcdcd',
+  boxShadow: '0 0 20px 0 rgba(255, 255, 255, 0.25)',
+  transform: `rotateX(${roll.value * 40}deg) rotateY(${tilt.value * 40}deg)`,
+
+}))
+</script>
---
+++ src/pages/parallax-effect.md
@@ -0,0 +1,42 @@
+---
+title: Parallax effect card
+---
+
+<div class="text-center">
+  <carbon-dicom-overlay class="text-4xl -mb-6 m-auto" />
+  <h3>Create parallax effect easily</h3>
+  <p>useParallax</p>
+</div>  
+
+effect inspired by [blackmagic.so](https://blackmagic.so/)
+
+<parallax-effect-demo/>
+
+
+## view
+```html
+ <div ref="container">
+    <div :style="cardStyle"></div>
+ </div>
+```
+
+## logic
+```javascript
+<script setup lang='ts'>
+import { useParallax } from '@vueuse/core'
+
+const container = ref<HTMLDivElement | null>(null)
+const { tilt, roll, source } = useParallax(container)
+const cardStyle = computed(() => ({
+  border: '1px solid #cdcdcd',
+  boxShadow: '0 0 20px 0 rgba(255, 255, 255, 0.25)',
+  transform: `rotateX(${roll.value * 40}deg) 
+              rotateY(${tilt.value * 40}deg)`,
+}))
+</script>
+
+```
+
+<span class="animate-pulse text-xl">
+✨
+</span>
