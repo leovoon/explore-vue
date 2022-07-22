f854b7b5a024a0f20424b3bb4f0ccbf260f15e44
leovoon <voonlihhaur@gmail.com>
2022-07-23 01:57:46 +0800

    update pnpm-lock, rm transition

--- src/App.vue
+++ src/App.vue
@@ -22,25 +22,9 @@ useHead({
 
 <template>
   <router-view v-slot="{ Component }">
-    <Transition name="scale" mode="out-in">
-      <component :is="Component"></component>
-    </Transition>
+    <component :is="Component"></component>
   </router-view>
   <ClientOnly>
     <AsyncReloadPrompt />
   </ClientOnly>
 </template>
-
-<style>
-.scale-enter-active,
-.scale-leave-active {
-  transition: all 0.3s ease;
-}
-
-.scale-enter-from,
-.scale-leave-to {
-  opacity: 0;
-  transform: scale(0.94);
-}
-
-</style>
